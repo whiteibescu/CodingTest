42ace1fb75308607a24bd0b5d1fa3ccd253e5dd1
SungSoo Park <whiteibescu@gmail.com>
2021-02-12 00:43:15 +0900

    Time Complexity using Clock header

---
+++ TimeComplexity/TimeComplexity/TimeComplexity_01.cpp
@@ -0,0 +1,28 @@
+//clock 함수를 이용한 방법
+
+#include <stdio.h>	
+#include <time.h>
+
+int main()
+{
+	clock_t start, end;
+	int result;
+	int sum = 0;
+
+	start = clock();
+
+	for (int i = 0; i < 10000; ++i)
+	{
+		for (int j = 0; j < 10000; ++j)
+		{
+			sum += i * j;
+		}
+	}
+
+	end = clock();
+
+	result = end - start;
+	printf("%d", result);
+
+	return 0;
+}
