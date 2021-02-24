e8ce856ed5ed82cc8825374ef35f058519a6e6ed
SungSoo Park <whiteibescu@gmail.com>
2021-02-24 23:25:31 +0900

    주석제거

--- Problem_CPP/Problem_CPP/TopBottomLeftRight.cpp
+++ Problem_CPP/Problem_CPP/TopBottomLeftRight.cpp
@@ -5,24 +5,24 @@
 
 using namespace std;
 
-// N�� �Է¹ޱ�
+
 int n;
 string plans;
 int x = 1, y = 1;
 
-// L, R, U, D�� ���� �̵� ���� 
+
 int dx[4] = { 0, 0, -1, 1 };
 int dy[4] = { -1, 1, 0, 0 };
 char moveTypes[4] = { 'L', 'R', 'U', 'D' };
 
 int main(void) {
     cin >> n;
-    cin.ignore(); // ���� ���� 
+    cin.ignore(); 
     getline(cin, plans);
-    // �̵� ��ȹ�� �ϳ��� Ȯ��
+    
     for (int i = 0; i < plans.size(); i++) {
         char plan = plans[i];
-        // �̵� �� ��ǥ ���ϱ� 
+        
         int nx = -1, ny = -1;
         for (int j = 0; j < 4; j++) {
             if (plan == moveTypes[j]) {
@@ -30,9 +30,9 @@ int main(void) {
                 ny = y + dy[j];
             }
         }
-        // ������ ����� ��� ���� 
+        
         if (nx < 1 || ny < 1 || nx > n || ny > n) continue;
-        // �̵� ���� 
+        
         x = nx;
         y = ny;
     }
