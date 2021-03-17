4c6f6efeffd37827e3a84d4747e22d87b299b658
SungSoo Park <whiteibescu@gmail.com>
2021-03-17 23:42:34 +0900

    GuessTheNumber(User)

--- Problem/GuessTheNumber.py
+++ Problem/GuessTheNumber.py
@@ -12,5 +12,21 @@ def guess(x):
 
     print(f"Yay, congrats. You have guessed the number {random_number} correctly!!")
 
-guess(10)
+def computer_guess(x):
+    low = 1
+    high = x
+    feedback = ""
+    while feedback != "c":
+        if low != high:
+            guess = random.randint(low, high)
+        else:
+            guess = low
+        feedback = input(f"Is {guess} too high (H), Too low (L), or correct (C)").lower
+        if feedback == "h":
+            high = guess - 1
+        elif feedback == "l":
+            low = guess + 1
 
+    print (f"Yay! The computer has guessed your number, {guess}, correctly!")
+
+computer_guess(10)
